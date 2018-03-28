1b534eebf837cf9ec26dc296062ec371340a33a3
vencax <vencax@centrum.cz>
2018-03-28 12:22:44 +0200

    fix

--- src/models.js
+++ src/models.js
@@ -1,10 +1,8 @@
+const CONSTS = require('./consts')
 
 module.exports = (models, bookshelf) => {
   //
   models.file = bookshelf.Model.extend({
-    tableName: 'proposals',
-    feedbacks: function () {
-      return this.hasMany(ProposalFeedback, 'proposalid')
-    }
+    tableName: CONSTS.TABLENAMES.FILES
   })
 }
